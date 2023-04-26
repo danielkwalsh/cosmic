91f61dfdcf382bdbb6191edefeac7bd989c9831c
vchanda2 <chandan.vikas@gmail.com>
2023-04-26 13:19:03 -0700

    Merging conflicting changes

--- cosmiclib.py
+++ cosmiclib.py
@@ -7,7 +7,6 @@ class Compressor:
         self.qmax = qmax
         self.wmin = wmin
         self.wmax = wmax
-
         self.qopt = None
         self.is_on = None
         self.wopt = None
@@ -69,3 +68,77 @@ class Optimizer:
         outdict['Success'] = (status == OptimizationStatus.OPTIMAL)
         outdict['OptimizationStatus'] = status
         return outdict
+ 
+ 
+# Need to move alpha, beta to the base compressor class   
+# Counterfactual stuff goes here
+class Counterfactualgenerator:
+    def __init__(self, compressors = []):
+        self.compressors = compressors
+        self.ncomps = len(self.compressors)
+        self.alpha = [] #  can be moved to the base class 
+        self.beta = [] # can be moved to the base class 
+        self.theta = []
+        self.q_cf = [None] * self.ncomps
+        self.w_cf = []
+        self.I = range(self.ncomps)
+        self.index_map = {c.name: i for i, c in enumerate(self.compressors)} # Creates an index map (maps compressor name to index)
+        self.flag = 1 # indicator of infeasibility (set to 0 when infeasible)
+
+    def find_counterfactual(self, Q, cf_option, sequence_cf = []): # sequence_cf is neeeded when cf_option = 1
+        Q_rem = Q
+        self.cf_sequence = sequence_cf # Preferred sequence set by the user
+        if Q > sum(self.compressors[i].qmax for i, c in enumerate(self.compressors)):
+            self.q_cf = [None] * self.ncomps
+            self.flag = 0
+        else:
+            if cf_option == 1: # smart CF (incorporates a preferred sequence)
+                for i, c in enumerate(self.cf_sequence):
+                    compressor_index = self.index_map[c]
+                    qmin_i = self.compressors[compressor_index].qmin
+                    qmax_i = self.compressors[compressor_index].qmax
+                    if 0 < Q_rem < qmin_i:
+                        self.q_cf = [None] * self.ncomps
+                        self.flag = 0
+                    elif Q_rem > qmax_i:
+                        self.q_cf[compressor_index] = qmax_i
+                        Q_rem = Q_rem- float(self.q_cf[compressor_index])                    
+                    else:
+                        self.q_cf[compressor_index] = Q_rem
+                        Q_rem = Q_rem- float(self.q_cf[compressor_index])
+            if cf_option == 2: # Less smart CF (distributes load evenly among compressors)
+                den = sum(self.compressors[i].qmax for i, c in enumerate(self.compressors)) # Sum of max capacitiies of all compressors 
+                infeasible_compressors = []
+                for i, c in enumerate(self.compressors): # This loop sets initial assignment 
+                    self.q_cf[i] = Q*c.qmax/den # Initial assignment of compressor load proportional to its max capacity
+                    if self.q_cf[i] < c.qmin: # checks feasibility of assignment 
+                        self.q_cf[i] = 0 # sets assigned load to zero for infeasible compressors
+                        infeasible_compressors.append(i) # Keeps track of infeasible compressors 
+                        den = den - c.qmax # adjusts denominator by removing infeasible compressors from the equation 
+                for i, c in enumerate(self.compressors): # This loop does reassignment accounting for infeasibility 
+                    if i in infeasible_compressors:
+                        self.q_cf[i] = Q*c.qmax/den
+            else:
+                print('wrong counterfactual option selected')
+
+        #return self.q_cf
+    
+    def post_process_counterfactual(self):
+        self. w_cf =  [None] * self.ncomps
+        self.theta_cf = [None] * self.ncomps
+        outdict_cf = {'compressors': dict()}
+        for i, c in enumerate(self.compressors):
+            self.alpha.append((c.wmax - c.wmin)/(c.qmax - c.qmin))
+            self.beta.append(c.wmin - c.qmin * self.alpha[i])
+            self.theta_cf[i] = None if self.q_cf[i] is None else (self.q_cf[i]>0)
+            self.w_cf[i] = None if self.q_cf[i] is None else self.alpha[i] * self.q_cf[i] + self.beta[i] - self.beta[i] * (1 - self.theta_cf[i])
+            outdict_cf['compressors'].update({c.name: {'theta': self.theta_cf[i], 'q': self.q_cf[i], 'w': self.w_cf[i]}})
+        outdict_cf['Qtot'] = None if self.flag == 0 else sum(self.q_cf)
+        outdict_cf['Wtot'] = None if self.flag == 0 else sum(self.w_cf)
+        outdict_cf['Success'] = (self.flag ==1)
+        outdict_cf['OptimizationStatus'] = None
+        return outdict_cf
+
+
+            
+    
